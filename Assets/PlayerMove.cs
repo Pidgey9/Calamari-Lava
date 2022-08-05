e458d0500ec6d427b01cbda95f9d8fb6ca2b00d4
Pidgey <bastien.bouchet@3wa.io>
2022-08-05 16:45:29 +0200

    last update before desktop?

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -9,6 +9,9 @@ public class PlayerMove : MonoBehaviour
     float rotate;
     public float speed;
     public float rspeed;
+    Vector2 moveBackward;
+    public int maxrota;
+    //public int maxspeed;
 
     private void Awake()
     {
@@ -18,12 +21,26 @@ public class PlayerMove : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float slow = Input.GetAxis("Slowdown");
         move = transform.up * v;
+        //moveBackward = transform.up * slow;
         rotate = -h;
     }
     private void FixedUpdate()
     {
         rb.velocity = move * speed;
+        /*if (rb.velocity <= )
+        {
+            rb.velocity = moveBackward;
+        }*/
         rb.angularVelocity = rotate * rspeed;
+        if (rb.angularVelocity > maxrota)
+        {
+            rb.angularVelocity = maxrota;
+        }
+        if (rb.angularVelocity < -maxrota)
+        {
+            rb.angularVelocity = -maxrota;
+        }
     }
 }
